9fe4a0e3975a789508e2d0527807d0c35e8c1b9e
unknown <edwardssevume@gmail.com>
2025-05-31 01:09:11 -0400

    Added another in feature branch

--- src/staging.java
+++ src/staging.java
@@ -1,5 +1,7 @@
 public class staging {
     public static void main(String[] args) {
+
         System.out.println("Welcome to branching");
+        System.out.println("Added feature branch");
     }
 }
